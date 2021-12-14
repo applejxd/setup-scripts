049271ab46aa63060bf5bbae6b6087c711f6e573
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-12-14 22:16:18 +0900

    update

--- softether-vpn/init.sh
+++ softether-vpn/init.sh
@@ -1,38 +1,66 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+    # Save Password
+    read -sp "Password: " password
+else
+    password=$1
+fi
+
+# Read internet configs
 read -sp "Input IP address for virtual hub: " hub_ip
 read -sp "Input IP address for router (gateway): " router_ip
 
-sudo apt update
+echo "$password" | sudo -S -E apt-get update
 
-mkdir -p ~/src
-cd ~/src
+################
+# Dependencies #
+################
 
+# for download
 if !(type "jq" >/dev/null 2>&1); then
     # for parse json
-    sudo apt install -y jq
+    echo "$password" | sudo -S -E apt-get install -y jq
 fi
-
-response=$(curl https://api.github.com/repos/SoftEtherVPN/SoftEtherVPN_Stable/releases/latest)
-download_url=$(echo $response | jq ".assets[38].browser_download_url" | sed 's/"//g')
-file_name=$(echo $response | jq ".assets[38].name" | sed 's/"//g')
-wget $download_url
-tar zxvf $file_name
 
 if !(type "make" >/dev/null 2>&1); then
     sudo apt install -y build-essential
 fi
 
+###########
+# Install #
+###########
+
+# Get download URL
+response=$(curl https://api.github.com/repos/SoftEtherVPN/SoftEtherVPN_Stable/releases/latest)
+download_url=$(echo $response | jq ".assets[38].browser_download_url" | sed 's/"//g')
+file_name=$(echo $response | jq ".assets[38].name" | sed 's/"//g')
+
+# Download
+mkdir -p ~/src
+cd ~/src
+wget $download_url
+tar zxvf $file_name
+
+# Build
 cd vpnserver
 yes 1 | make
 find . -type f -print | xargs chmod 600
 find . -type d -print | xargs chmod 700
 chmod u+x .install.sh vpncmd vpnserver
+
+# Install
 cd ~/src
 sudo cp -rp vpnserver /usr/local/
 sudo chown -R root:root /usr/local/vpnserver/
 
-sudo tee /etc/systemd/system/vpnserver.service <<EOF >/dev/null
+###########
+# systemd #
+###########
+
+# cf. https://qiita.com/rimksky/items/e169f9af83ce472b4ce3
+
+echo "$password" | sudo -S -E tee /etc/systemd/system/vpnserver.service <<EOF >/dev/null
 [Unit]
 Description=SoftEther VPN Server
 After=network.target
@@ -47,9 +75,17 @@ ExecReload=/opt/vpnserver.sh restart
 WantedBy=multi-user.target
 EOF
 
-script_contents=$(curl -L https://raw.githubusercontent.com/applejxd/softether-setup/main/vpnserver.sh | sed -e "s/HUB_IP/$hub_ip/g" -e "s/ROUTER_IP/$router_ip/g")
+##########
+# init.d #
+##########
+
+script_contents=$(curl -L https://raw.githubusercontent.com/applejxd/softether-setup/main/vpnserver_template.sh | sed -e "s/HUB_IP/$hub_ip/g" -e "s/ROUTER_IP/$router_ip/g")
 echo $script_contents | sudo tee /opt/vpnserver.sh
 
-sudo systemctl daemon-reload
-sudo systemctl enable vpnserver
-sudo systemctl start vpnserver
+###########
+# Service #
+###########
+
+echo "$password" | sudo -S -E systemctl daemon-reload
+echo "$password" | sudo -S -E systemctl enable vpnserver
+echo "$password" | sudo -S -E systemctl start vpnserver
